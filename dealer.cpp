dfe77df58ada07a68d3143e0e51f8ad86e109dc0
MyahJS <myahj1@umbc.edu>
2023-12-04 23:20:13 -0500

    editing insert and insert rehash

--- dealer.cpp
+++ dealer.cpp
@@ -6,24 +6,23 @@ CarDB::CarDB(int size, hash_fn hash, prob_t probing = DEFPOLCY){
     // postconditions: a new hash table is created
     //  make sure the size of the table is a prime number between MAXPRIME and MINPRIME
     //  allocate memory for the table and initialize all member variables
-    if (MINPRIME<=size<=MAXPRIME) {
-        if (isPrime(size)){
-            m_currentCap = size;
-        } else {
-            m_currentCap = findNextPrime(size);
-        }
+    if (size < MINPRIME){
+        m_currentCap = MINPRIME;
+    } else if (size > MAXPRIME){
+        m_currentCap = MAXPRIME;
     } else {
-        m_currentCap = (size<MINPRIME) ? MINPRIME : MAXPRIME;
+        m_currentCap = findNextPrime(size);
     }
+        
     m_hash = hash;
+    m_newPolicy = NONE;
+
     m_currProbing = probing;
     m_currentTable = new Car[m_currentCap];
     for (int i = 0; i < m_currentCap; ++i)
         m_currentTable[i] = EMPTY;
     m_currentSize = 0;
     m_currNumDeleted = 0;
-
-    m_newPolicy = NONE;
 
     m_oldTable = nullptr;
     m_oldCap = 0;
@@ -36,14 +35,10 @@ CarDB::~CarDB(){
     // Destructor
     // preconditions: hash table exists
     // postconditions: deallocate memory
-    if (m_currentTable){
+    if (m_currentTable)
         delete[] m_currentTable;
-        m_currentTable = nullptr;
-    } 
-    if (m_oldTable){
-        delete[] m_oldTable;
-        m_oldTable = nullptr;
-    }  
+    if (m_oldTable)
+        delete[] m_oldTable;  
 }
 
 void CarDB::changeProbPolicy(prob_t policy){
@@ -59,39 +54,35 @@ bool CarDB::insert(Car car){
     // postconditions:
 
     // check if id is valid
-    if (!(MINID<=car.getDealer()<=MAXID))
+    if (car.getDealer() < MINID || car.getDealer() > MAXID)
         return false;
 
     // find first index using hash function
-    int index = m_hash(car.getModel());
+    int index = m_hash(car.getModel()) % m_currentCap;
+    int org_index = index;
 
-    if (index>=m_currentCap)
-        return false;
-
-    // check if index is occupied
-    if (m_currentTable[index]==EMPTY){
-        m_currentTable[index] = car;
-        m_currentSize++;
-    } else {
-        // find an unoccupied index using the specified probing strategy
-        int i = 0;
-        if (m_currProbing==QUADRATIC){
-            while (!(m_currentTable[index]==EMPTY)){
-                if (m_currentTable[index]==car) // check for dup
-                    return false;
-                index = ((m_hash(car.getModel()) % m_currentCap) + (i*i)) % m_currentCap;
-                i++;
-            }
-            m_currentTable[index] = car;
-        } else {
-            while (!(m_currentTable[index]==EMPTY)){
-                if (m_currentTable[index]==car) // check for dup
-                    return false;
-                index = ((m_hash(car.getModel()) % m_currentCap) + i * (11 - (m_hash(car.getModel()) % 11))) % m_currentCap;
-                i++;
-            }
-            m_currentTable[index] = car;
+    // find an unoccupied index using the specified probing strategy
+    int i = 0;
+    if (m_currProbing==QUADRATIC){
+        while (!(m_currentTable[index]==EMPTY)){
+            if (i>=m_currentCap)
+                return false;
+            if (m_currentTable[index]==car) // check for dup
+                return false;
+            index = ((org_index % m_currentCap) + (i*i)) % m_currentCap;
+            i++;
         }
+        m_currentTable[index] = car;
+    } else {
+        while (!(m_currentTable[index]==EMPTY)){
+            if (i>=m_currentCap)
+                return false;
+            if (m_currentTable[index]==car) // check for dup
+                return false;
+            index = ((org_index % m_currentCap) + i * (11 - (org_index % 11))) % m_currentCap;
+            i++;
+        }
+        m_currentTable[index] = car;
     }
 
     // rehash here 
@@ -117,20 +108,20 @@ bool CarDB::insert(Car car){
             int portion = m_oldSize/4;
             int i = 0;
             int j = 0;
-            int index;
             while (j<portion){
                 if (m_oldTable[i].getUsed()==true){
                     // insert into new table
-                    index = m_hash(m_oldTable[i].getModel());
+                    index = m_hash(m_oldTable[i].getModel()) % m_currentCap;
+                    org_index = index;
                     int k = 0;
                     if (m_currProbing==QUADRATIC){
                         while (!(m_currentTable[index]==EMPTY)){
-                            index = ((m_hash(m_oldTable[i].getModel()) % m_currentCap) + (k*k)) % m_currentCap;
+                            index = ((org_index % m_currentCap) + (k*k)) % m_currentCap;
                             k++;
                         }
                     } else {
                         while (!(m_currentTable[index]==EMPTY)){
-                            index = ((m_hash(m_oldTable[i].getModel()) % m_currentCap) + k * (11-(m_hash(m_oldTable[i].getModel()) % 11))) % m_currentCap;
+                            index = ((org_index % m_currentCap) + k * (11-(org_index % 11))) % m_currentCap;
                             k++;
                         }
                     }
@@ -152,20 +143,20 @@ bool CarDB::insert(Car car){
         }
         int i = 0;
         int j = m_currentSize;
-        int index;
         while (j<portion){
             if (m_oldTable[i].getUsed()==true){
                 // insert into new table
-                index = m_hash(m_oldTable[i].getModel());
+                index = m_hash(m_oldTable[i].getModel()) % m_currentCap;
+                org_index = index;
                 int k = 0;
                 if (m_currProbing==QUADRATIC){
                     while (!(m_currentTable[index]==EMPTY)){
-                        index = ((m_hash(m_oldTable[i].getModel()) % m_currentCap) + (k*k)) % m_currentCap;
+                        index = ((org_index % m_currentCap) + (k*k)) % m_currentCap;
                         k++;
                     }
                 } else {
                     while (!(m_currentTable[index]==EMPTY)){
-                        index = ((m_hash(m_oldTable[i].getModel()) % m_currentCap) + k * (11-(m_hash(m_oldTable[i].getModel()) % 11))) % m_currentCap;
+                        index = ((org_index % m_currentCap) + k * (11-(org_index % 11))) % m_currentCap;
                         k++;
                     }
                 }
